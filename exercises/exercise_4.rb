2b532a070f941ccc262bd4f618b87132d05d8bbc
richard135 <richardhsieh1@hotmail.com>
2017-03-14 20:52:40 +0000

    Excercise four of Rails DB

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,3 +7,18 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+store = Store.create(name: "surrey", annual_revenue:224000, mens_apparel:false, womens_apparel:true)
+store = Store.create(name: "Whistler", annual_revenue:1900000, mens_apparel:true, womens_apparel:false)
+store = Store.create(name: "Yaletown", annual_revenue:430000, mens_apparel:true, womens_apparel:true)
+
+puts Store.count
+
+@mens_store = Store.where("mens_apparel = true")
+@mens_store.each do |store|
+  puts "#{store.name} - #{store.annual_revenue}"
+end
+
+@womens_store = Store.where("womens_apparel = true", "annual_revenue < 1000000")
+@womens_store.each do |store|
+  puts "Womens store: #{store.name} - #{store.annual_revenue}"
+end
